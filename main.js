3f59df79701a789eaaca7a7ccd2337131586bf61
TheBogdichHD <nottoowellconstructed@gmail.com>
2024-11-06 13:04:43 +0300

    feat: setup project

---
+++ main.js
@@ -0,0 +1,19 @@
+main();
+
+function main() {
+  /** @type {HTMLCanvasElement} */
+  const canvas = document.querySelector("#gl-canvas");
+  /** @type {WebGLRenderingContext} */
+  const gl = canvas.getContext("webgl");
+
+  if (gl === null) {
+    alert(
+      "Unable to initialize WebGL. Your browser or machine may not support it.",
+    );
+    return;
+  }
+
+  gl.clearColor(0.0, 0.0, 0.0, 1.0);
+
+  gl.clear(gl.COLOR_BUFFER_BIT);
+}
