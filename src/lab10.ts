e3c567d60c008c3944916bae5a38a5b86f92c042
Владислав Крылов <krylov@sfedu.ru>
2024-11-12 20:06:44 +0300

    feat(lab10.ts, green-triangle.html): lab10 done

---
+++ src/lab10.ts
@@ -0,0 +1,102 @@
+const vertexShaderString: string = `#version 300 es
+in vec2 coord;
+
+void main() {
+  gl_Position = vec4(coord, 0.0, 1.0);
+}
+`;
+
+const fragmentShaderString: string = `#version 300 es
+precision highp float;
+
+out vec4 color;
+
+void main() {
+  // the TRIANGLE is GREEN
+  color = vec4(0, 1, 0, 1.0);
+}
+`;
+
+function initShader(gl: WebGL2RenderingContext, type: number, source: string): WebGLShader | undefined {
+  const shader = gl.createShader(type);
+  if (!shader) return undefined;
+
+  gl.shaderSource(shader, source);
+  gl.compileShader(shader);
+  if (gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+    return shader;
+  }
+  console.error(gl.getShaderInfoLog(shader));
+  gl.deleteShader(shader);
+  return undefined;
+}
+
+function createProgram(gl: WebGL2RenderingContext, vertexShader: WebGLShader, fragmentShader: WebGLShader): WebGLProgram | undefined {
+  const program = gl.createProgram();
+  if (!program) return undefined;
+
+  gl.attachShader(program, vertexShader);
+  gl.attachShader(program, fragmentShader);
+  gl.linkProgram(program);
+
+  if (gl.getProgramParameter(program, gl.LINK_STATUS)) {
+    return program;
+  }
+
+  console.error(gl.getProgramInfoLog(program));
+  gl.deleteProgram(program);
+  return undefined;
+}
+
+function main() {
+  const canvas = document.querySelector<HTMLCanvasElement>("#gl-canvas")!;
+  const gl = canvas.getContext("webgl2") as WebGL2RenderingContext;
+  if (!gl) {
+    alert("No WebGL2 for YOU!");
+    return;
+  }
+
+  const vertexShader = initShader(gl, gl.VERTEX_SHADER, vertexShaderString);
+  const fragmentShader = initShader(gl, gl.FRAGMENT_SHADER, fragmentShaderString);
+
+  if (!vertexShader || !fragmentShader) {
+    alert("VertexShader or FragmentShader are not initialized!");
+    return;
+  }
+
+  const program = createProgram(gl, vertexShader, fragmentShader);
+  if (!program) {
+    alert("Program is not initialized!");
+    return;
+  }
+
+  const vertexAttributeLocation = gl.getAttribLocation(program, "coord");
+  if (vertexAttributeLocation === -1) {
+    alert("Something is wrong with getAttribLocation for coord!");
+    return;
+  }
+
+  const VBO = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, VBO);
+
+  const triangle = [
+    -1.0, -1.0,
+    0.0, 1.0,
+    1.0, -1.0,
+  ];
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(triangle), gl.STATIC_DRAW);
+
+  gl.useProgram(program);
+  gl.enableVertexAttribArray(vertexAttributeLocation);
+  gl.bindBuffer(gl.ARRAY_BUFFER, VBO);
+
+  gl.vertexAttribPointer(vertexAttributeLocation, 2, gl.FLOAT, false, 0, 0);
+  gl.clearColor(0, 0, 0, 1);
+  gl.clear(gl.COLOR_BUFFER_BIT);
+  gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+
+  gl.drawArrays(gl.TRIANGLES, 0, 3);
+  gl.bindBuffer(gl.ARRAY_BUFFER, null);
+}
+
+main();
