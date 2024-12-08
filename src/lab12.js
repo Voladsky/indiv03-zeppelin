167fa04ed5fc816bbe1d07be54f0f0f573f5ed21
TheBogdichHD <nottoowellconstructed@gmail.com>
2024-12-08 22:22:15 +0300

    feat: texture suffering

--- src/lab12.js
+++ src/lab12.js
@@ -122,20 +122,29 @@ class Cube {
         const vertexShader = `#version 300 es
         in vec3 position;
         in vec3 color;
+        in vec2 t;
+
         out vec3 v_color;
+        out vec2 tex_coord;
         uniform mat4 u_modelViewProjectionMatrix;
+
         void main() {
             vec4 pos = u_modelViewProjectionMatrix * vec4(position, 1.0);
             gl_Position = pos;
             v_color = color;
+            tex_coord = t;
         }
         `;
         const fragmentShader = `#version 300 es
         precision lowp float;
         in vec3 v_color;
         out vec4 color;
+
+        in vec2 tex_coord;
+        uniform sampler2D our_texture;
+        
         void main() {
-            color = vec4(v_color, 1.0);
+            color = texture(our_texture, tex_coord) * vec4(v_color, 1.0);
         }
         `;
         let program = this.app.createProgram(vertexShader, fragmentShader);
@@ -169,22 +178,53 @@ class Cube {
      */
     initVBO() {
         const vertexData = new Float32Array([
-            // Triangle 1
-            0.0, 0.8, 0.0, 1.0, 0.0, 0.0,       // Top
-            -0.8, -0.8, -0.8, 0.0, 1.0, 0.0,    // Bottom left-back
-            0.8, -0.8, -0.8, 0.0, 0.0, 1.0,     // Bottom right-back
-            // Triangle 2
-            0.0, 0.8, 0.0, 1.0, 0.0, 0.0,       // Top
-            -0.8, -0.8, -0.8, 0.0, 1.0, 0.0,    // Bottom left-back
-            0.0, -0.8, 0.8, 1.0, 1.0, 0.0,      // Bottom front-center
-            // Triangle 3
-            0.0, 0.8, 0.0, 1.0, 0.0, 0.0,       // Top
-            0.8, -0.8, -0.8, 0.0, 0.0, 1.0,     // Bottom right-back
-            0.0, -0.8, 0.8, 1.0, 1.0, 0.0,      // Bottom front-center
-            // Triangle 4
-            -0.8, -0.8, -0.8, 0.0, 1.0, 0.0,    // Bottom left-back
-            0.8, -0.8, -0.8, 0.0, 0.0, 1.0,     // Bottom right-back
-            0.0, -0.8, 0.8, 1.0, 1.0, 0.0       // Bottom front-center
+            // Front face
+            -0.5, -0.5, +0.5, 1.0, 0.0, 0.0,  // Red
+            -0.5, +0.5, +0.5, 0.0, 0.0, 1.0,  // Blue
+            +0.5, +0.5, +0.5, 0.0, 0.0, 1.0,  // Blue
+            +0.5, +0.5, +0.5, 0.0, 0.0, 1.0,  // Blue
+            +0.5, -0.5, +0.5, 1.0, 0.0, 0.0,  // Red
+            -0.5, -0.5, +0.5, 1.0, 0.0, 0.0,  // Red
+
+            // Back face
+            -0.5, -0.5, -0.5, 1.0, 0.0, 0.0,  // Red
+            +0.5, +0.5, -0.5, 0.0, 0.0, 1.0,  // Blue
+            -0.5, +0.5, -0.5, 0.0, 0.0, 1.0,  // Blue
+            +0.5, +0.5, -0.5, 0.0, 0.0, 1.0,  // Blue
+            -0.5, -0.5, -0.5, 1.0, 0.0, 0.0,  // Red
+            +0.5, -0.5, -0.5, 1.0, 0.0, 0.0,  // Red
+
+            // Top face
+            -0.5, +0.5, -0.5, 0.0, 0.0, 1.0,  // Blue
+            -0.5, +0.5, +0.5, 0.0, 0.0, 1.0,  // Blue
+            +0.5, +0.5, +0.5, 0.0, 0.0, 1.0,  // Blue
+            +0.5, +0.5, +0.5, 0.0, 0.0, 1.0,  // Blue
+            +0.5, +0.5, -0.5, 0.0, 0.0, 1.0,  // Blue
+            -0.5, +0.5, -0.5, 0.0, 0.0, 1.0,  // Blue
+
+            // Bottom face
+            -0.5, -0.5, -0.5, 1.0, 0.0, 0.0,  // Red
+            +0.5, -0.5, +0.5, 1.0, 0.0, 0.0,  // Red
+            -0.5, -0.5, +0.5, 1.0, 0.0, 0.0,  // Red
+            +0.5, -0.5, +0.5, 1.0, 0.0, 0.0,  // Red
+            -0.5, -0.5, -0.5, 1.0, 0.0, 0.0,  // Red
+            +0.5, -0.5, -0.5, 1.0, 0.0, 0.0,  // Red
+
+            // Right face
+            +0.5, -0.5, -0.5, 1.0, 0.0, 0.0,  // Red
+            +0.5, -0.5, +0.5, 1.0, 0.0, 0.0,  // Red
+            +0.5, +0.5, +0.5, 0.0, 0.0, 1.0,  // Blue
+            +0.5, +0.5, +0.5, 0.0, 0.0, 1.0,  // Blue
+            +0.5, +0.5, -0.5, 0.0, 0.0, 1.0,  // Blue
+            +0.5, -0.5, -0.5, 1.0, 0.0, 0.0,  // Red
+
+            // Left face
+            -0.5, -0.5, -0.5, 1.0, 0.0, 0.0,  // Red
+            -0.5, +0.5, +0.5, 0.0, 0.0, 1.0,  // Blue
+            -0.5, -0.5, +0.5, 1.0, 0.0, 0.0,  // Red
+            -0.5, +0.5, +0.5, 0.0, 0.0, 1.0,  // Blue
+            -0.5, -0.5, -0.5, 1.0, 0.0, 0.0,  // Red
+            -0.5, +0.5, -0.5, 0.0, 0.0, 1.0,  // Blue
         ]);
         const VBO = this.gl.createBuffer();
         this.gl.bindBuffer(this.gl.ARRAY_BUFFER, VBO);
@@ -195,43 +235,72 @@ class Cube {
         const colorLocation = this.gl.getAttribLocation(this.program, "color");
         this.gl.enableVertexAttribArray(colorLocation);
         this.gl.vertexAttribPointer(colorLocation, 3, this.gl.FLOAT, false, 6 * Float32Array.BYTES_PER_ELEMENT, 3 * Float32Array.BYTES_PER_ELEMENT);
+
+
+        const image = new Image();
+        image.onload = () =>
+        {
+            var texture_1 = this.gl.createTexture();
+            this.gl.bindTexture(this.gl.TEXTURE_2D, texture_1);
+    
+            this.gl.texImage2D(texture_1, 0, 3, image.width, image.height, 0, this.gl.RGB, this.gl.UNSIGNED_BYTE, image.data);
+            this.gl.activeTexture(texture_1);
+
+            const textureLocation = this.gl.getUniformLocation(this.program, "our_texture");
+            this.gl.uniform1i(textureLocation, 0)
+
+            const textureCoordLocation = this.gl.getAttribLocation(this.program, "tex_coord");
+            this.gl.enableVertexAttribArray(textureCoordLocation);
+            this.gl.vertexAttribPointer(textureCoordLocation, 2, this.gl.FLOAT, false, 8 * Float32Array.BYTES_PER_ELEMENT, 6 * Float32Array.BYTES_PER_ELEMENT);
+        };
+        image.src = "../images/master_of_puppets.jpg";       
     }
     render() {
         this.gl.clear(this.gl.COLOR_BUFFER_BIT | this.gl.DEPTH_BUFFER_BIT);
         this.initCamera();
-        this.gl.drawArrays(this.gl.TRIANGLES, 0, 12);
+        this.gl.drawArrays(this.gl.TRIANGLES, 0, 36);
     }
     translate(dx, dy, dz) {
         mat4.translate(this.translationMatrix, this.translationMatrix, [dx, dy, dz]);
     }
 };
 
-function main() {
-    let tetrahedron = new Tetrahedron("#gl-canvas");
-    window.addEventListener('keydown', function (e) {
-        switch (e.code) {
-            case "ArrowLeft":
-                tetrahedron.translate(-0.1, 0, 0);
-                break;
-            case "ArrowRight":
-                tetrahedron.translate(0.1, 0, 0);
-                break;
-            case "ArrowUp":
-                tetrahedron.translate(0, 0.1, 0);
-                break;
-            case "ArrowDown":
-                tetrahedron.translate(0, -0.1, 0);
-                break;
-            case "KeyW":
-                tetrahedron.translate(0, 0, 0.1);
-                break;
-            case "KeyS":
-                tetrahedron.translate(0, 0, -0.1);
-                break;
-        }
-        tetrahedron.render(); // Re-render after translation
-    });
+var figure = new Tetrahedron("#gl-canvas");
 
-    tetrahedron.render();
-}
-main();
+var selectFigure = document.querySelector('#figure');
+selectFigure.addEventListener('change', function() {
+    if (selectFigure.options[0].selected === true) {
+        figure = new Tetrahedron("#gl-canvas");
+    }
+    else if (selectFigure.options[1].selected === true) {
+        figure = new Cube("#gl-canvas");
+    }
+
+    figure.render();
+});
+
+window.addEventListener('keydown', function (e) {
+    switch (e.code) {
+        case "ArrowLeft":
+            figure.translate(-0.1, 0, 0);
+            break;
+        case "ArrowRight":
+            figure.translate(0.1, 0, 0);
+            break;
+        case "ArrowUp":
+            figure.translate(0, 0.1, 0);
+            break;
+        case "ArrowDown":
+            figure.translate(0, -0.1, 0);
+            break;
+        case "KeyW":
+            figure.translate(0, 0, 0.1);
+            break;
+        case "KeyS":
+            figure.translate(0, 0, -0.1);
+            break;
+    }
+    figure.render();
+});
+
+figure.render();
